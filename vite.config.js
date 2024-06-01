ebc07692045c9c0deb1a40d782e7eb89336d5be8
HDD <gptsemail.com@gmail.com>
2024-06-01 23:02:54 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HDD-TEAM/',
+  base: '/HDD-TEAM/', // Укажите ваш репозиторий
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
